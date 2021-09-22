8af3488cad23545775f160eb2d9ff01de51462f1
Mateusz Jabłoński <mail@mateuszjablonski.com>
2021-09-22 13:02:54 +0200

    add type

--- src/components/ArticlePreview/ArticlePreview.types.ts
+++ src/components/ArticlePreview/ArticlePreview.types.ts
@@ -1,7 +1,11 @@
+import { Asset } from 'contentful';
+
 export interface ArticlePreviewProps {
     title: string;
     excerpt: any;
     slug: string;
     createdDate: string;
-    featuredImage: object;
+    featuredImage: {
+        image: Asset;
+    };
 }
