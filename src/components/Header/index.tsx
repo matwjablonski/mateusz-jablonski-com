7a568dce7fb402e907dca31e61fbf0b50d660a08
Mateusz Jabłoński <mail@mateuszjablonski.com>
2021-10-02 02:41:01 +0200

    menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import Link from 'next/link';
+import Grid from '../Grid';
+import styles from './Header.module.scss';
+import MainNav from '../MainNav';
 
 const Header: FunctionComponent = () => {
 
   return (
-    <header>
-      nagłówek
-    </header>
+    <Grid>
+      <header className={styles.header}>
+        <div>Logo</div>
+        <MainNav />
+      </header>
+    </Grid>
   )
 }
 
---
+++ src/components/MainNav/index.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react';
+import Link from 'next/link';
+import menuData from '../../data/main-menu.json';
+import Button from '../Button';
+import { ButtonType } from '../Button/Button.types';
+import styles from './MainNav.module.scss';
+
+const MainNav: FunctionComponent = () => {
+    return (
+        <nav className={styles.nav}>
+          <ul className={styles.list}>
+            {menuData.items.map(item => <li key={item.id} className={styles.item}>
+              { 
+                item.type === 'link' && (
+                  <Link href={item.url}>
+                    <a title={item.title}>
+                      {item.title}
+                    </a>
+                  </Link>
+                )
+              }
+              {
+                item.type === 'button' && (
+                  <Button.L pattern={ButtonType.SECONDARY} label={item.title} href={item.url}/>
+                )
+              }
+            </li>)}
+          </ul>
+        </nav>
+    )
+}
+
+export default MainNav;
