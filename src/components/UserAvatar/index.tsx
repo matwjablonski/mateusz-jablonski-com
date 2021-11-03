1569c1410a468cdd0125e396afbbc8dc4a437101
Mateusz Jabłoński <mail@mateuszjablonski.com>
2021-11-03 07:19:49 +0100

    add gravatars for comments

--- src/components/UserAvatar/index.tsx
+++ src/components/UserAvatar/index.tsx
@@ -2,15 +2,15 @@ import React, { FunctionComponent, useEffect } from 'react';
 import Image from 'next/image';
 import { UserAvatarProps } from './UserAvatar.types';
 import md5 from 'md5';
+import styles from './UserAvatar.module.scss';
 
-const UserAvatar: FunctionComponent<UserAvatarProps> = ({ email }) => {
-
-    return <div>
+const UserAvatar: FunctionComponent<UserAvatarProps> = ({ email }) => (
+    <div className={styles.userAvatar}>
         { email ? 
-            <Image src={`https://gravatar.com/${md5(email)}`} width={64} height={64} /> :
-            <div>adadada</div>
+            <Image src={`https://gravatar.com/avatar/${md5(email)}`} width={64} height={64} className={styles.avatar} /> :
+            <div className={styles.avatarPlaceholder} />
         }
     </div>
-}
+)
 
 export default UserAvatar
