7bdadb355fa2edcfc8d7af9776e6d3f7ebd23017
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-02-07 08:59:00 +0100

    fix problem with noscroll after nav

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import cx from 'classnames';
 import { useRouter } from 'next/router'
 import Head from 'next/head';
@@ -14,6 +14,12 @@ const mainDescription = "Blog, podcast oraz kursy o programowaniu i rozwoju. Ofe
 
 const MainLayout: FunctionComponent<{ head: HeadInterface, hideOverflow?: boolean }> = ({children, head, hideOverflow}) => {
   const { asPath } = useRouter();
+
+  useEffect(() => {
+    if (window !== undefined) {
+      document.body.classList.remove('noscroll');
+    }
+  }, [])
 
   return (
     <>
