b808d3980c1da31a34b1f85728ec68e103b97b6b
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-02-10 10:31:15 +0100

    change title of testimonials

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -6,7 +6,7 @@ import TestimonialBox from "../TestimonialBox";
 const Testimonials: FC<TestimonialsProps> = ({ testimonials }) => {
     return (
         <div className={styles.testimonials}>
-            <h3 className={styles.testimonialsTitle}>Co mówią ludzie?</h3>
+            <h3 className={styles.testimonialsTitle}>Co mówią o mojej pracy?</h3>
             <p className={styles.testimonialsText}>Największą wartością dla mnie jest zadowolenie moich odbiorców: czytelników, słuchaczy, widzów i osób, których pomagam realizować ich cele. Poniżej znajdziesz kilka wybranych opinii na temat mojej pracy.</p>
             <div className={styles.testimonialsList}>
                 {testimonials.map(testimonial => <TestimonialBox key={testimonial.name} testimonial={testimonial} />)}
