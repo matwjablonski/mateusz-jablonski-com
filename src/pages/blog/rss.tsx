987dbdcf8d5af2bba467a2d6d3ac7f4872467e73
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-03-05 06:49:24 +0100

    rss feed

---
+++ src/pages/blog/rss.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { NextPageContext } from "next";
+import { fetchEntries } from '../../contentful'
+import { Article } from "../../types/common/Article.types";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+const getBlogRssXml = (articles: Article[]) => {
+    let rssItemsXml = "";
+    articles.forEach(post => {
+        const excerpt = documentToReactComponents(post.excerpt, {});
+
+        rssItemsXml += `
+            <item>
+                <title>${post.title}</title>
+                <link>https://mateuszjablonski.com/blog/${post.slug}</link>
+                <pubDate>${post.createdDate}</pubDate>
+                <description>
+                <![CDATA[${excerpt[0].props.children[0]}]]>
+                </description>
+            </item>`;
+    });
+
+    return {
+      rssItemsXml,
+      latestArticleDate: articles[0].createdDate,
+    };
+};
+  
+const getRssXml = (articles: Article[]) => {
+    const { rssItemsXml, latestArticleDate } = getBlogRssXml(articles);
+    return `<?xml version="1.0" ?>
+        <rss version="2.0">
+            <channel>
+                <title>Mateusz Jabłoński</title>
+                <link>https://mateuszjablonski.com</link>
+                <description>test</description>
+                <language>pl</language>
+                <lastBuildDate>${latestArticleDate}</lastBuildDate>
+                ${rssItemsXml}
+            </channel>
+        </rss>
+    `;
+};
+  
+
+export default class Rss extends React.Component {
+    static async getInitialProps({ res }: NextPageContext) {
+        if (!res) {
+            return;
+        }
+        const articlesRes = await fetchEntries({
+            content_type: 'article',
+            include: 2,
+            order: '-fields.createdDate',
+        });
+
+        const content = getRssXml(articlesRes.data.map(p => p.fields));
+        res.setHeader("Content-Type", "text/xml");
+        res.write(content);
+        res.end();
+    }
+}
