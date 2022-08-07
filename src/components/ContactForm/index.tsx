eeeb3ad9dadc544c49e288097fb822f6fd50585e
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-08-08 01:49:03 +0200

    success message when form is submitted

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -50,11 +50,12 @@ const prefferedForms = [
 ];
 
 const ContactForm = () => {
-    const { register, handleSubmit, setError, formState: { errors } } = useForm({
+    const { register, handleSubmit, setError, reset, formState: { errors } } = useForm({
         resolver: yupResolver(schema),
     });
     const { executeRecaptcha } = useGoogleReCaptcha();
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
     const onSubmit = useCallback((data) => {
         setIsSubmitting(true);
@@ -67,70 +68,84 @@ const ContactForm = () => {
             .then((gReCaptchaToken) => {
                 fetch('/api/email/send', { method: 'POST', body: JSON.stringify({ ...data, gReCaptchaToken }) })
                     .finally(() => {
+                        reset();
                         setIsSubmitting(false);
+                        setIsSubmitted(true);
                     })
             });
     }, [executeRecaptcha])
 
     return (
         <div className={styles.contactForm}>
-            <h2 className={styles.formTitle}>Formularz kontaktowy</h2>
-            <p className={styles.formText}>Dla twojej wygody formularz skrócony jest do jak najmniejszej liczby punktów, tak abym jak najszybciej i jak najsprawniej skontaktował się z Tobą.</p>
-            <form onSubmit={handleSubmit(onSubmit)} method="POST" noValidate>
-                <InputWrapper label="Wybierz temat:">
-                    <RadioButtonsGroup>
-                        {possibleTopics.map(({ value, label }) => (
-                            <RadioButton
-                                register={register}
-                                name="topic"
-                                key={value}
-                                value={value}
-                                label={label}
-                            />
-                        ))}
-                    </RadioButtonsGroup>
-                    <MessageWrapper messageType={MessageType.ERROR}>
-                        <ErrorMessage errors={errors} name={'topic'} />
-                    </MessageWrapper>
-                </InputWrapper>
-                <InputWrapper label="Imię:">
-                    <input
-                        {...register('name')}
-                        type="text"
-                        placeholder="Podaj swoje imię"
-                    />
-                    <MessageWrapper messageType={MessageType.ERROR}>
-                        <ErrorMessage errors={errors} name={'name'} />
-                    </MessageWrapper>
-                </InputWrapper>
-                <InputWrapper label="Adres e-mail:">
-                    <input
-                        {...register('email')}
-                        type="email"
-                        placeholder="Podaj adres e-mail" 
-                    />
-                    <MessageWrapper messageType={MessageType.ERROR}>
-                        <ErrorMessage errors={errors} name={'email'} />
-                    </MessageWrapper>
-                </InputWrapper>
-                <InputWrapper label="Preferowany kontakt:">
-                    <RadioButtonsGroup>
-                        {prefferedForms.map(({ value, label }) => (
-                            <RadioButton 
-                                value={value}
-                                key={value}
-                                name="prefferedForm"
-                                register={register}
-                                label={label}
-                            />
-                        ))}
-                    </RadioButtonsGroup>
-                    <MessageWrapper messageType={MessageType.ERROR}>
-                        <ErrorMessage errors={errors} name={'prefferedForm'} />
-                    </MessageWrapper>
-                </InputWrapper>
-                <Button.B type="submit" label="Napisz do mnie" pattern={ButtonType.PRIMARY} disabled={isSubmitting}/>
-            </form>
+            {
+                !isSubmitted ? (
+                    <>
+                        <h2 className={styles.formTitle}>Formularz kontaktowy</h2>
+                        <p className={styles.formText}>Dla twojej wygody formularz skrócony jest do jak najmniejszej liczby punktów, tak abym jak najszybciej i jak najsprawniej skontaktował się z Tobą.</p>
+                        <form onSubmit={handleSubmit(onSubmit)} method="POST" noValidate>
+                            <InputWrapper label="Wybierz temat:">
+                                <RadioButtonsGroup>
+                                    {possibleTopics.map(({ value, label }) => (
+                                        <RadioButton
+                                            register={register}
+                                            name="topic"
+                                            key={value}
+                                            value={value}
+                                            label={label}
+                                        />
+                                    ))}
+                                </RadioButtonsGroup>
+                                <MessageWrapper messageType={MessageType.ERROR}>
+                                    <ErrorMessage errors={errors} name={'topic'} />
+                                </MessageWrapper>
+                            </InputWrapper>
+                            <InputWrapper label="Imię:">
+                                <input
+                                    {...register('name')}
+                                    type="text"
+                                    placeholder="Podaj swoje imię"
+                                />
+                                <MessageWrapper messageType={MessageType.ERROR}>
+                                    <ErrorMessage errors={errors} name={'name'} />
+                                </MessageWrapper>
+                            </InputWrapper>
+                            <InputWrapper label="Adres e-mail:">
+                                <input
+                                    {...register('email')}
+                                    type="email"
+                                    placeholder="Podaj adres e-mail" 
+                                />
+                                <MessageWrapper messageType={MessageType.ERROR}>
+                                    <ErrorMessage errors={errors} name={'email'} />
+                                </MessageWrapper>
+                            </InputWrapper>
+                            <InputWrapper label="Preferowany kontakt:">
+                                <RadioButtonsGroup>
+                                    {prefferedForms.map(({ value, label }) => (
+                                        <RadioButton 
+                                            value={value}
+                                            key={value}
+                                            name="prefferedForm"
+                                            register={register}
+                                            label={label}
+                                        />
+                                    ))}
+                                </RadioButtonsGroup>
+                                <MessageWrapper messageType={MessageType.ERROR}>
+                                    <ErrorMessage errors={errors} name={'prefferedForm'} />
+                                </MessageWrapper>
+                            </InputWrapper>
+                            <Button.B type="submit" label="Napisz do mnie" pattern={ButtonType.PRIMARY} disabled={isSubmitting}/>
+                        </form>
+                    </>
+                ) : (
+                    <div className={styles.successContent}>
+                        <h2 className={styles.successTitle}>Wiadomość została wysłana.</h2>
+                        <p className={styles.successText}>Teraz Twoja wiadomość dociera do mnie na skrzynkę, gdy tylko otrzymam powiadomienie postaram się odpowiedzieć jak najszybciej. Ale dla pewności daj mi maksymalnie 24 godziny.</p>
+                        <Button.L label="Super, zabierz mnie na stronę główną" pattern={ButtonType.PRIMARY} href="/"/>
+                    </div>
+                )
+            }
         </div>
     )
 }
